677c4ff30d6494d34515255000b9de9f951b9948
covy2406 <nguyentranthaovy2406@gmail.com>
2024-02-27 01:57:39 +0700

    setup project

---
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+
+function Layout() {
+  return (
+    <div>
+      <Header/>
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+export default Layout;
