5a94e177ec9596e6e040e95303f6cfddb492be9a
covy2406 <nguyentranthaovy2406@gmail.com>
2024-02-27 17:03:42 +0700

    setup project...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Header = () => {
-
-    return (
-        <>
-        <h1>Hello</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
 export default Header;
