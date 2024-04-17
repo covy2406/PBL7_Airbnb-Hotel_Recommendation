28aa84dbe11b33268d1443c7d06b1238c5814c14
covy2406 <nguyentranthaovy2406@gmail.com>
2024-04-17 13:46:19 +0700

    home base

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import Layout from './layouts/DefaultLayout/DefaultLayout';
-import Home from './pages/Home/Home';
 import HeaderOnlyLayout from './layouts/HeaderOnlyLayout/HeaderOnlyLayout';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import HomePage from './pages/Home/Home';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path='/' element={<Layout/>}>
-          <Route index element={<Home/>}/>
+          <Route index element={<HomePage/>}/>
         </Route>
         <Route path="/login" element={<HeaderOnlyLayout />}>
           <Route index element={<Login></Login>} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,47 +1,44 @@
-import React from 'react';
+import React from "react";
+import ImageSlider from "./ImageSlider";
+import ImageHotel from '../../assets/images/ImageHotel.jpg'
 
-const HomePage = ({ title, image, price }) => {
-  console.log(title);
+const Home = () => {
+  const imageList = [
+    { id: 1, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 2, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 3, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 4, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 5, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 6, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 7, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 8, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 9, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 10, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 11, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 12, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 13, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 14, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+    { id: 15, title: "JWMariot, Sai Gon", image: ImageHotel, price: 1541 },
+  ];
+  console.log('in ra ket qua tai day',imageList[0].title)
   return (
-    <>
-      <div className='my-28'>
-        <div className="relative ">
-          <div className="grad absolute w-full h-full rounded-b-[1.3rem]"></div>
-          <div className="flex ">
-            {/* Background */}
-            <img
-              src={image}
-              alt="ảnh"
-              className="object-cover rounded-[1.3rem] sm:h-[17rem] md:h-[13rem] w-full"
-            />
-            {/* Title */}
-            {/* <div className="absolute text-white font-bold bottom-6 left-6 text-[22px] flex items-center gap-2">
-              {title}
-              <span>&#x2022;</span>
-              <p className="text-[18px] text-slate-200"> {price}</p>
-            </div> */}
-          </div>
-        </div>
-        {/* Description */}
-        <div className="flex items-start justify-between pt-3">
-          {/* Left */}
-          <div className="">
-            {/* <p className="max-w-[17rem] font-semibold text-[17px]">
-              This is a rare find
-            </p> */}
-            <h5 className="max-w-[17rem]  text-[16px] -mt-1 text-gray-500">
-              15/4 - 17/4
-            </h5>
-            <h5 className="max-w-[17rem] font-semibold text-[16px] -mt-1 text-gray-500">{price}</h5>
-          </div>
-          {/* Right */}
-          <div className="flex items-center space-x-1">
-            {/* <BsStarFill /> */}
-            <h5 className="text-[15px]">5.0</h5>
-          </div>
-        </div>
+    <div className=" sm:py-5">
+      <div className="z-0 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {imageList.map((imgItem) => (
+          <ImageSlider
+            key={imgItem.id}
+            title={imgItem.title}
+            image={imgItem.image} 
+            price={imgItem.price} 
+          />
+        ))}
       </div>
-    </>
+    </div>
   )
 };
-export default HomePage;
+
+export default Home;
+
+
+
+
