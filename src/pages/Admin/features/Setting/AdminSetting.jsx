180e4cae503ab47b6940c2cc0f648b51cf680f22
Haizues1210 <104113556+Lithesys@users.noreply.github.com>
2024-04-26 11:39:43 +0700

    setting up

---
+++ src/pages/Admin/features/Setting/AdminSetting.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AdminSetting = () => {
+  return (
+    <>
+      <div>AdminSetting</div>
+    </>
+  );
+};
---
+++ src/pages/Admin/features/index.js
@@ -0,0 +1,3 @@
+export * from './Crawling';
+export * from './Dashboard';
+export * from './Setting';
